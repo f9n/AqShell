13ad3d50812faf914e2190541a182eae86f8ff46
pleycpl <h4rvey@protonmail.com>
2017-10-30 20:56:39 +0000

    added new 3.party function

--- main.c
+++ main.c
@@ -9,6 +9,50 @@
 #include <termios.h>
 
 #define INPUT_LEN 100
+#define ARROW_UP 72
+#define ARROW_DOWN 80
+#define ARROW_LEFT 75
+#define ARROW_RIGHT 77
+
+/*
+    # getch() and getche() in linux
+    https://stackoverflow.com/questions/3276546/how-to-implement-getch-function-of-c-in-linux
+*/
+static struct termios old, new;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo) {
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  new = old; /* make new settings same as old settings */
+  new.c_lflag &= ~ICANON; /* disable buffered i/o */
+  new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void) {
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo) {
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void) {
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void) {
+  return getch_(1);
+}
+
 
 void startingMenu() {
     char* starting = "Starting the Project";
@@ -59,6 +103,7 @@ int execute(char* string) {
  http://www.it.iitb.ac.in/frg/wiki/images/3/38/P13324_Sandeep_Prasad_Week_3_Report_6_Input_Without_Pressing_Enter.pdf
  https://faq.cprogramming.com/cgi-bin/smartfaq.cgi?answer=1045691686&id=1043284392/
  */
+/*
  char getch() {
     char buf = 0;
     struct termios old = {0};
@@ -78,22 +123,30 @@ int execute(char* string) {
             perror ("tcsetattr ~ICANON");
     return (buf);
 }
-
+*/
+void displayPrompt(void) {
+    printf("> ");
+}
 int main() {
     char * input = malloc(sizeof(char) * INPUT_LEN);
+    char first;
     // startingMenu();
-    char first = getch();
-    printf("%c > ", first);
-    /*
+    while(1) {
+        displayPrompt();
+        first = getch();
+        printf("%c", first);
+        /*
         # Read with spaces in scanf
         http://www.c4learn.com/c-programming/c-reading-string-with-spaces-scanf/
         https://stackoverflow.com/questions/13726499/how-can-i-scan-strings-with-spaces-in-them-using-scanf
-    */
-    scanf("%99[^\n]s", input+1);
-    input[0] = first;
-    printf("%s\n", input);
-    int result = execute(input);
-    printf("%d\n", result);
+        */
+        scanf("%99[^\n]s", input+1);
+        input[0] = first;
+        printf("%s\n", input);
+        int result = execute(input);
+        printf("%d\n", result);
+        first = 0;
+    }
 
     free(input);
     return 0;
