d44b316c971849db5a5c6143873a5f60e6b9f649
pleycpl <h4rvey@protonmail.com>
2017-11-02 19:55:54 +0000

    seperated execute() function

---
+++ execute.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include "execute.h"
+
+int execute(char* string) {
+   // concat 2 string.
+   // First string is "cd Pwd";
+   // Second string is "ls -la"; 
+    pid_t pid;
+    /* fork another process */
+    pid = fork();
+    if (pid < 0) { /* error occurred */
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    } else if (pid == 0) { /* child process */
+        execlp("/bin/bash", "/bin/bash", "-c", string, NULL);
+    } else { /* parent process */
+        /* parent will wait for the child */
+        wait(NULL);
+        return 0;
+    }
+}
---
+++ execute.h
@@ -0,0 +1,5 @@
+#ifndef __EXECUTE_H__
+    #define __EXECUTE_H__
+
+    int execute(char* string);
+#endif
