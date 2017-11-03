75e752083c702144cbdf463fa93394d9a43d7caa
pleycpl <h4rvey@protonmail.com>
2017-11-03 21:35:47 +0000

    added isDeleted flag

--- shell.c
+++ shell.c
@@ -34,73 +34,71 @@ void ShellMenu(void) {
   int index = 0;
   int isDeleted = 1; // 1 is False and 0 is True. isDeleted = deleted shell command status.
   /*
-	Tasks:
+		Tasks:
 		Parse input. Check exists 'cd' command in input. and then modify _PWD variable
-		Check exit 
-		Ctrl+L control!
-		Signals control! like Ctrl+C and Ctrl+D
+		Check exit
   */
   while(1) {
-	if (index == 0 && isDeleted == 1) {
-		displayPrompt(_USERNAME, _HOSTNAME, _PWD);
-	}
-	CharInput = getch();
-	// Up, Down, Left, Right key
-	if (CharInput == 27) {
-		CharInput = getch();
-		if (CharInput == 91) {
-			CharInput = getch();
-			switch(CharInput) {
-			case 65:
-				printf("UP\n");
-				break;
+    if (index == 0 && isDeleted == 1) {
+		    displayPrompt(_USERNAME, _HOSTNAME, _PWD);
+		  }
+    CharInput = getch();
+    // Up, Down, Left, Right key
+    if (CharInput == 27) {
+      CharInput = getch();
+      if (CharInput == 91) {
+        CharInput = getch();
+        switch(CharInput) {
+        case 65:
+          printf("UP\n");
+          break;
 
-			case 66:
-				printf("DOWN\n");
-				break;
+        case 66:
+          printf("DOWN\n");
+          break;
 
-			case 67:
-				printf("RIGHT\n");
-				break;
+        case 67:
+          printf("RIGHT\n");
+          break;
 
-			case 68:
-				printf("LEFT\n");
-				break;
-			
-			default :
-				printf("Oh no!\n");
-			}
-		}
+        case 68:
+          printf("LEFT\n");
+          break;
+
+        default :
+          printf("Oh no!\n");
+        }
+      }
     } else if (CharInput == ENTER) {
-		//printf("Pressed Enter key!\n");
-		printf("\n");
-		isDeleted = 1;
-		if (index != 0) {
-			//SaveCommandToHistory(input);
-			result = execute(input);
-			index = 0;
-			memset(input, 0, INPUT_LEN); // set empty
-		}
+     //printf("Pressed Enter key!\n");
+      printf("\n");
+      isDeleted = 1;
+      if (index != 0) {
+        //SaveCommandToHistory(input);
+        result = execute(input);
+        index = 0;
+        memset(input, 0, INPUT_LEN); // set empty
+      }
     } else if (CharInput == BACKSPACE) {
-		//printf("Pressed BackSpace\n");
-		isDeleted = 0;
-		if (index != 0) {
-			printf("\b \b");
-			index--;
-			input[index] = 0;
-		}
+      //printf("Pressed BackSpace\n");
+      isDeleted = 0;
+      if (index != 0) {
+        printf("\b \b");
+        index--;
+        input[index] = 0;
+      }
     } else if (CharInput == HORIZONTAL_TAB || CharInput == VERTIVAL_TAB) { // Horizontal Tab or Vertical Tab
-	  printf("Pressed Horizontal Tab\n");
+      printf("Pressed Horizontal Tab\n");
     } else if (CharInput == CONTROL_L) {
-	  system("clear");
+      system("clear");
     } else if (CharInput == CONTROL_C || CharInput == CONTROL_D) {
-	  exit(0);
-    }else {
-	  printf("%c", CharInput);
-	  input[index] = CharInput;
-	  index++;
+      exit(0);
+    } else {
+      printf("%c", CharInput);
+      input[index] = CharInput;
+      index++;
     }
   }// while
 
-  free(input);
+ free(input);
 }
