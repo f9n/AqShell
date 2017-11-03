8c16460703ca4ecfb76249374864e0dd2a3bc80e
pleycpl <h4rvey@protonmail.com>
2017-11-03 15:28:03 +0000

    added ctrl+c signal, we kill child process

--- execute.c
+++ execute.c
@@ -4,13 +4,21 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <signal.h>
 #include "execute.h"
 
+pid_t pid = -1 ; //Global
+
+void kill_child(int sig) {
+  kill(pid, SIGKILL);
+}
+
 int execute(char* string) {
+    signal(SIGINT,(void (*)(int))kill_child);
    // concat 2 string.
    // First string is "cd Pwd";
    // Second string is "ls -la"; 
-    pid_t pid;
+    //pid_t pid;
     /* fork another process */
     pid = fork();
     if (pid < 0) { /* error occurred */
