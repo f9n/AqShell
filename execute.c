fa61966f356b96360fa516240dce0eec8c31098c
pleycpl <h4rvey@protonmail.com>
2017-11-04 10:53:05 +0000

    added exit command

--- execute.c
+++ execute.c
@@ -13,19 +13,21 @@ void kill_child(int sig) {
   kill(pid, SIGKILL);
 }
 
-int execute(char* string) {
+int execute(char* command) {
     signal(SIGINT,(void (*)(int))kill_child);
+    if (strcmp(command, "exit") == 0) {
+      exit(0);
+    }
    // concat 2 string.
    // First string is "cd Pwd";
-   // Second string is "ls -la"; 
-    //pid_t pid;
+   // Second string is "ls -la";
     /* fork another process */
     pid = fork();
     if (pid < 0) { /* error occurred */
         fprintf(stderr, "Fork Failed");
         return 1;
     } else if (pid == 0) { /* child process */
-        execlp("/bin/bash", "/bin/bash", "-c", string, NULL);
+        execlp("/bin/bash", "/bin/bash", "-c", command, NULL);
     } else { /* parent process */
         /* parent will wait for the child */
         wait(NULL);
